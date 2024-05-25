504049852cb06fc307db97a5715a620aede078fe
qduong753951 <quocduong852456@gmail.com>
2024-05-25 23:46:46 +0700

    V3.4.1

--- script/main.js
+++ script/main.js
@@ -27,6 +27,35 @@ closeNavHeader.onclick=()=>{
     navheader.classList.add("translate-l");
 }
 
+console.log(document.querySelector(".seconds").innerHTML);
+
+const countdown = ()=>{
+    const now = new Date().getTime();
+    const countDownDate = now + 7 * 24 * 60 * 60 * 1000; // 7 days from now
+
+    const x = setInterval(function() {
+    const now = new Date().getTime();
+
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelector(".days").innerHTML = days < 10 ? '0' + days : days;
+    document.querySelector(".hours").innerHTML = hours < 10 ? '0' + hours : hours;
+    document.querySelector(".minutes").innerHTML = minutes < 10 ? '0' + minutes : minutes;
+    document.querySelector(".seconds").innerHTML = seconds < 10 ? '0' + seconds : seconds;
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+    }
+    }, 1000);
+}
+
+
+
 function main(){
     if(document.querySelector('#new-item-next-btn')){
         document.querySelector('#new-item-next-btn').onclick = function(){
@@ -41,6 +70,7 @@ function main(){
         }
     }
     handleScroll();
+    countdown();
 }
 
 main()  
